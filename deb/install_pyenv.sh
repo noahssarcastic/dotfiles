b258694738dcf7ddf3fbf20cd73d8be88207ff79
Noah Iniguez <niniguezg@gmail.com>
2022-11-20 21:22:43 -0600

    Restructure dotfiles

---
+++ deb/install_pyenv.sh
@@ -0,0 +1,22 @@
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+sudo apt-get update; sudo apt-get install \
+    build-essential \
+    curl \
+    libbz2-dev \
+    libffi-dev \
+    liblzma-dev
+    libncursesw5-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    llvm \
+    make \
+    tk-dev \
+    wget \
+    xz-utils \
+    zlib1g-dev
+
+# cd ~/.pyenv && src/configure && make -C src
--- deb/ssh-clip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt install xclip
-ssh-keygen -t ed25519 -C "$1"
-xclip -sel clip < ~/.ssh/id_ed25519.pub
